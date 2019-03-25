733e4cfd4983e30949656b0ed4031c5dbd91b4ff
X_Worm <vasilkindiy@gmail.com>
2019-03-25 17:38:48 +0200

    Added realization of Student class
    This class abstracts the student, which includes fields of the student's name and level of knowledge

---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿namespace InternshipTest.Person
+{
+    public class Student
+    {
+        private string StudentName; // Name of the student
+        private Knowledge StudentKnowleage; // Student Knowledge level
+
+        
+
+        public Student(string name)
+        {
+            this.StudentName = name; // Set Student name 
+            StudentKnowleage = new Knowledge(0); // Set default student knowledge
+        }
+
+        public Student(string name , int level)
+        {
+            this.StudentName = name; // Set Student name 
+            StudentKnowleage = new Knowledge(level); // Set default student knowledge
+        }
+
+        public void SetKnowledge(Knowledge knowledge)
+        {
+            this.StudentKnowleage = knowledge;
+        }
+
+        public int GetKnowelageLevel()
+        {
+            return this.StudentKnowleage.GetLevel();
+        }
+
+        public string GetName()
+        {
+            return StudentName;
+        }
+    }
+}
