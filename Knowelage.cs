f12118c8fa169f9fc02c578770c348674c0165e9
X_Worm <vasilkindiy@gmail.com>
2019-03-25 17:32:08 +0200

    Added realization of Knowelage class
    This class is created to keep the student's level of knowledge

---
+++ Knowelage.cs
@@ -0,0 +1,17 @@
+﻿namespace InternshipTest
+{
+    public class Knowledge
+    {
+        private int KnowleageLevel; // Level of the student knowelage
+
+        public Knowledge(int level) // Constructor with 1 parameter
+        {
+            this.KnowleageLevel = level;
+        }
+        
+        public int GetLevel() // return student Knowleage level
+        {
+            return KnowleageLevel;
+        }
+    }
+}
